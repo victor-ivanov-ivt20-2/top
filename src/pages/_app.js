8ebff7abb39e17a1692909361a351b2227437bf3
seqku <134935486+seqku@users.noreply.github.com>
2023-07-18 13:38:51 +0300

    feat/initial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@600&family=Mulish:wght@700&display=swap');
+</style>
+import { Mulish } from 'next/font/google'
+import styles from "/src/styles/styles.css"
+const mulish = Mulish({ subsets: ['latin'] })
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={mulish.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
